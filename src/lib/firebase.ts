65bbb2d0c71dc9492c8bab80e4649a2c8afb1cbd
Harley Knox <therealharleyknox@gmail.com>
2025-07-21 08:03:21 +0000

    modified:   src/lib/firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -49,12 +49,12 @@ import { generateInitialAnalysis, type GenerateInitialAnalysisInput } from "@/ai
 
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  apiKey: "AIzaSyDjoi3EJ7l1dcOF1JRPlzC6rQg2DDGyCH8",
+  authDomain: "pollitgo.firebaseapp.com",
+  projectId: "pollitgo",
+  storageBucket: "pollitgo.firebasestorage.app",
+  messagingSenderId: "1078455480838",
+  appId: "1:1078455480838:web:6fc2d68bd37853373c0987"
 };
 
 let app: FirebaseApp;
