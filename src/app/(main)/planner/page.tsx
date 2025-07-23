a72616200ccfb1882675b216e8ac0732ab24cefe
Harley Knox <therealharleyknox@gmail.com>
2025-07-23 10:46:05 +0000

    Running "vercel build" Vercel CLI 44.5.0 Installing dependencies... up t

--- src/app/(main)/planner/page.tsx
+++ src/app/(main)/planner/page.tsx
@@ -42,7 +42,7 @@ export default function PlannerPage() {
                         <CrmTaskList />
                     </CardContent>
                 </Card>
-            </aside>
+            </div>
         </div>
     </div>
   );
